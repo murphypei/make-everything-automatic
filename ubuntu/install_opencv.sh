4a80e96164c2219b5a4d86fe3414297ef7c259a3
murphy <murphypei@163.com>
2022-11-22 16:58:03 +0800

    update install_opencv.sh

--- ubuntu/install_opencv.sh
+++ ubuntu/install_opencv.sh
@@ -1,6 +1,3 @@
-set -e
-set -x
-
 ######################################
 # INSTALL OPENCV ON UBUNTU OR DEBIAN #
 ######################################
@@ -10,7 +7,7 @@ set -x
 # ---------------------------------------------------------------------|
 OPENCV_VERSION='4.5.1' # Version to be installed
 OPENCV_CONTRIB='YES'   # Install OpenCV's extra modules (YES/NO)
-OPENCV_INSTALL='./opencv_install'
+OPENCV_INSTALL='./install'
 # -------------------------------------------------------------------- |
 
 # |          THIS SCRIPT IS TESTED CORRECTLY ON          |
@@ -27,67 +24,85 @@ OPENCV_INSTALL='./opencv_install'
 
 # 1. KEEP UBUNTU OR DEBIAN UP TO DATE
 
-sudo apt-get -y update
-# sudo apt-get -y upgrade       # Uncomment to install new versions of packages currently installed
-# sudo apt-get -y dist-upgrade  # Uncomment to handle changing dependencies with new vers. of pack.
-# sudo apt-get -y autoremove    # Uncomment to remove packages that are now no longer needed
+# sudo apt-get -y update
+# # sudo apt-get -y upgrade       # Uncomment to install new versions of packages currently installed
+# # sudo apt-get -y dist-upgrade  # Uncomment to handle changing dependencies with new vers. of pack.
+# # sudo apt-get -y autoremove    # Uncomment to remove packages that are now no longer needed
 
-# 2. INSTALL THE DEPENDENCIES
+# # 2. INSTALL THE DEPENDENCIES
 
-# Build tools:
-sudo apt-get install -y build-essential cmake
+# # Build tools:
+# sudo apt-get install -y build-essential cmake
 
-# GUI (if you want GTK, change 'qt5-default' to 'libgtkglext1-dev' and remove '-DWITH_QT=ON'):
-sudo apt-get install -y qt5-default libvtk6-dev
+# # GUI (if you want GTK, change 'qt5-default' to 'libgtkglext1-dev' and remove '-DWITH_QT=ON'):
+# sudo apt-get install -y qt5-default libvtk6-dev
 
-# Media I/O:
-sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev \
-    libopenexr-dev libgdal-dev
+# # Media I/O:
+# sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev jasper \
+#     libopenexr-dev libgdal-dev
 
-# Video I/O:
-sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev \
-    libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm \
-    libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+# # Video I/O:
+# sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev \
+#     libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm \
+#     libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
 
-# Parallelism and linear algebra libraries:
-sudo apt-get install -y libtbb-dev libeigen3-dev
+# # Parallelism and linear algebra libraries:
+# sudo apt-get install -y libtbb-dev libeigen3-dev
 
-# Python:
-sudo apt-get install -y python-dev python-tk pylint python-numpy \
-    python3-dev python3-tk pylint3 python3-numpy flake8
+# # Python:
+# sudo apt-get install -y python-dev python-tk pylint python-numpy \
+#     python3-dev python3-tk pylint3 python3-numpy flake8
 
-# Java:
-sudo apt-get install -y ant default-jdk
+# # Java:
+# sudo apt-get install -y ant default-jdk
 
-# Documentation and other:
-sudo apt-get install -y doxygen unzip wget
+# # Documentation and other:
+# sudo apt-get install -y doxygen unzip wget
 
-# 3. INSTALL THE LIBRARY
+# # 3. INSTALL THE LIBRARY
 
-wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
-unzip ${OPENCV_VERSION}.zip && rm ${OPENCV_VERSION}.zip
-mv opencv-${OPENCV_VERSION} OpenCV
+# wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
+# unzip ${OPENCV_VERSION}.zip && rm ${OPENCV_VERSION}.zip
+# mv opencv-${OPENCV_VERSION} OpenCV
 
-if [ $OPENCV_CONTRIB = 'YES' ]; then
-    wget https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
-    unzip ${OPENCV_VERSION}.zip && rm ${OPENCV_VERSION}.zip
-    mv opencv_contrib-${OPENCV_VERSION} opencv_contrib
-    mv opencv_contrib OpenCV
-fi
+# if [ $OPENCV_CONTRIB = 'YES' ]; then
+#     wget https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
+#     unzip ${OPENCV_VERSION}.zip && rm ${OPENCV_VERSION}.zip
+#     mv opencv_contrib-${OPENCV_VERSION} opencv_contrib
+#     mv opencv_contrib OpenCV
+# fi
 
-cd OpenCV && mkdir build && cd build
+cd OpenCV && rm -rf build && mkdir build
+cd build
+mkdir -p ${OPENCV_INSTALL}
 
 if [ $OPENCV_CONTRIB = 'NO' ]; then
-    cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON \
-        -DWITH_XINE=ON -DENABLE_PRECOMPILED_HEADERS=OFF \
-        -DCMAKE_INSTALL_PREFIX=${OPENCV_INSTALL} ..
+    cmake -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=${OPENCV_INSTALL} \
+        -DWITH_QT=ON \
+        -DWITH_OPENGL=ON \
+        -DFORCE_VTK=ON \
+        -DWITH_TBB=ON \
+        -DWITH_GDAL=ON \
+        -DWITH_XINE=ON \
+        -DENABLE_PRECOMPILED_HEADERS=OFF \
+        ..
 fi
 
 if [ $OPENCV_CONTRIB = 'YES' ]; then
-    cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON \
-        -DWITH_XINE=ON -DENABLE_PRECOMPILED_HEADERS=OFF \
+    cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_CXX_FLAGS="-std=c++14 -stdlib=libc++" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=${OPENCV_INSTALL} \
+        -DWITH_QT=ON \
+        -DBUILD_TIFF=ON \
+        -DBUILD_ZLIB=ON \
+        -DWITH_TBB=ON \
+        -DBUILD_EXAMPLES=OFF \
+        -DBUILD_PERF_TESTS=OFF \
+        -DBUILD_TESTS=OFF \
+        -DBUILD_DOCS=OFF \
         -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
-        -DCMAKE_INSTALL_PREFIX=${OPENCV_INSTALL} ..
+        ..
 fi
 
 make -j8
